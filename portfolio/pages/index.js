61240e5566c7f91de13acf1430d27038bd84725a
a.falei <august3@mail.ru>
2023-11-15 23:49:00 +0300

    createPortfolio

--- portfolio/pages/index.js
+++ portfolio/pages/index.js
@@ -8,6 +8,7 @@ export default function Home() {
         <div className={style.imgfoto}></div>
         <div className={style.info}>
           <h1>Falei Alina</h1>
+          <p>FullStack Developer</p>
           <p>Based: Republic of Belarus</p>
           <p>Phone: +37529-7771362</p>
           <p>E-mail: august3@mail.ru</p>
@@ -17,26 +18,33 @@ export default function Home() {
       <div className={style.title}>Work Experience</div>
       <div className={style.job}>
         <div className={style.jobBlock}>
-          <h1>Job title</h1>
-          <h2>company</h2>
-          <h2>time period</h2>
-          <p>Describe what your job entailed, what you did at your position.</p>
+          <h1>Software Engineer</h1>
+          <h2>HSC Technology</h2>
+          <h2>1 year</h2>
+          <p>Participation in the full development cycle starting from design, logic implementation and testing to the deployment and maintenance.</p>
         </div>
       </div>
 
       <div className={style.title}>Skills</div>
       <div className={style.skils}>
+
         <div className={style.block}>
-          <p>Wireframing</p>
-          <p>Prototyping</p>
-          <p>Mobile Application Design</p>
-          <p>Responsive Web Design</p>
+          <p>JavaScript</p>
+          <p>TypeScript</p>
+          <p>Node.js</p>
+          <p>React</p>
         </div>
         <div className={style.block}>
-          <p>Wireframing</p>
-          <p>Prototyping</p>
-          <p>Mobile Application Design</p>
-          <p>Responsive Web Design</p>
+          <p>Express.js</p>
+          <p>MongoDB</p>
+          <p>MySQL</p>
+          <p>PostgreSQL</p>
+        </div>
+        <div className={style.block}>
+          <p>SQL</p>
+          <p>Redux</p>
+          <p>Redux Toolkit</p>
+          <p>Git</p>
         </div>
       </div>
 
@@ -44,18 +52,27 @@ export default function Home() {
         <div className={style.educationBlock}>
           <div className={style.imgEducation}></div>
           <div className={style.education}>
-            <h1>Education</h1>
-            <p>1. test 1</p>
-            <p>2. test 1</p>
+            <p>Education</p>
+            <p>1. Belarusian State</p>
+            <p>Technological University</p>
+            <p>2. HSchool</p>
           </div>
         </div>
         <div className={style.languageBlock}>
           <div className={style.imgLanguage}></div>
           <div className={style.language}>
-            <h1>Language</h1>
+            <p>Language</p>
             <p>English B1</p>
             <p>Russian C2</p>
           </div>
+        </div>
+      </div>
+
+      <div className={style.portfolio}>
+        <div className={style.qr}></div>
+        <div className={style.infoPortfolio}>
+          <h1>my portfolio on github</h1>
+          <a href="https://github.com/faleialina">https://github.com/faleialina</a>
         </div>
       </div>
 
